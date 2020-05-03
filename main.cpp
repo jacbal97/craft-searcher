0c1e3ddaac34bc13594ca7e5759f326cd51c43ea
jacbal97 <48021758+jacbal97@users.noreply.github.com>
2020-05-03 03:10:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,6 @@ int main()
 	std::string a;
 	std::getline(std::cin,a);
 	search(&head, a).print();
-	//red_inner_backpack.print();
-	//std::cout << std::endl;
-	//neon_decoction.print();
-
-
+	
 	return 1;
 }
